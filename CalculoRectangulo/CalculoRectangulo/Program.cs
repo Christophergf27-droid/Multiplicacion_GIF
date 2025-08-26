f9def20c2f78717fb3bb19ca0f1b982b31ae4b5a
Christophergf27-droid <piero.gf27@gmail.com>
2025-08-26 17:08:49 -0500

    Add files via upload

---
+++ CalculoRectangulo/CalculoRectangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoRectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baserectangulo, altura, area;
+            Console.WriteLine("ingrese la base del rectangulo: ");
+            baserectangulo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingerese la altura del rectangulo: ");
+            altura = int.Parse(Console.ReadLine());
+
+            area = baserectangulo * altura;
+            Console.WriteLine("el area del rectangulo es: " + area);
+        }
+    }
+}
